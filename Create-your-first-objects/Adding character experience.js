195ffababe996e77055efb2ab3a147d435dccb32
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-22 19:52:03 -0400

    addedinstructions

--- Create-your-first-objects/Adding character experience.js
+++ Create-your-first-objects/Adding character experience.js
@@ -1,1 +1,12 @@
-//test2
+// TODO: create the character object here
+
+// Aurora is harmed by an arrow
+aurora.health -= 20;
+
+// Aurora equips a strength necklace
+aurora.strength += 10;
+
+// Aurora learn a new skill
+aurora.xp += 15;
+
+console.log(aurora.describe());
