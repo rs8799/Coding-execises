a02dda5928b1dafc836b1d25ce2b58be2add593c
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-22 20:00:54 -0400

    finishedcode

--- Create-your-first-objects/Adding character experience.js
+++ Create-your-first-objects/Adding character experience.js
@@ -1,5 +1,13 @@
 // TODO: create the character object here
-
+const aurora = {
+  name:"Aurora",
+  health: 150,
+  strength:25,
+  xp: 30,
+  describe(){
+    return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp}`;
+  }
+};
 // Aurora is harmed by an arrow
 aurora.health -= 20;
 
@@ -10,3 +18,17 @@ aurora.strength += 10;
 aurora.xp += 15;
 
 console.log(aurora.describe());
+
+
+
+/* const aurora = {
+  name: "Aurora",
+  health: 150,
+  strength: 25,
+
+  // Return the character description
+  describe() {
+    return `${this.name} has ${this.health} health points and ${this
+      .strength} as strength`;
+  }
+}; */
