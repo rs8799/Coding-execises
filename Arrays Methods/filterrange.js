32c3bd65a4dfb2e79a32b5dc74648c1a4a7f9751
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-23 19:13:10 -0400

    works!

--- Arrays Methods/filterrange.js
+++ Arrays Methods/filterrange.js
@@ -12,12 +12,26 @@ alert( filtered ); // 3,1 (matching values)
 
 alert( arr ); // 5,3,8,1 (not modified) */
 
-
 function filterRange(arr,a,b){
-  return arr.filter(item => (item <= b && item >= a));
+  return arr.filter(item => item <=b && item <=a)
 }
 
-let arr = [5,3,8,1];
-let filtered = filterRange(arr,1,4);
-alert(filtered);
-alert(arr);
+let arr = [6,10,20,1]
+let arrNew = filterRange(arr,5,1)
+console.log(arr)
+console.log(arrNew)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
