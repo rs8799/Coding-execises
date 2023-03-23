44101e455db392381bc7105c69797318c731588c
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-22 20:10:27 -0400

    finishedcodeworks

--- Create-your-first-objects/Modeling a dog.js
+++ Create-your-first-objects/Modeling a dog.js
@@ -1,4 +1,12 @@
 // TODO: create the dog object here
+const dog = {
+  name:"Rex",
+  species:"German Shepard",
+  size:"huge",
+  bark(){
+    return "Grr Grr"
+  }
+};
 
 console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
 console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
