d1c9576563fe58358363ef419afec4c5bdda7fc1
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-22 20:16:01 -0400

    initalcode

---
+++ Create-your-first-objects/Modeling a circle.js
@@ -0,0 +1,6 @@
+const r = Number(prompt("Enter the circle radius:"));
+
+// TODO: create the circle object here
+
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
