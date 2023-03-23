d334980c792b36853ae2029763ff11e2762a11e9
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-22 20:31:28 -0400

    codeworks

--- Create-your-first-objects/Modeling a circle.js
+++ Create-your-first-objects/Modeling a circle.js
@@ -1,5 +1,11 @@
 const r = Number(prompt("Enter the circle radius:"));
-
+const circle ={area(){
+  return Math.PI*r*r
+},
+  circumference(){
+    return 2 * Math.PI * r
+  }
+}
 // TODO: create the circle object here
 
 console.log(`Its circumference is ${circle.circumference()}`);
