cd34557f08343c3718182431c14d85428af82e55
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-23 21:08:44 -0400

    works!

---
+++ Arrays Methods/copy and sort array.js
@@ -0,0 +1,21 @@
+/* We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+
+Create a function copySorted(arr) that returns such a copy.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (no changes) */
+
+function copySorted(arr){
+  return arr.slice().sort()
+}
+
+let arr = ["HTML","Poppys", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted );
+alert( arr ); 
